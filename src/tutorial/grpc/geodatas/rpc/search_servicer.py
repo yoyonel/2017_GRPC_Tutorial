dc5dc4bc75527296171ac73a788b1aff591165be
ATTY Lionel <yoyonel@hotmail.com>
2019-02-23 03:18:29 +0100

    Modify (TODO: add) service to search point in table commune de france 2019

--- src/tutorial/grpc/geodatas/rpc/search_servicer.py
+++ src/tutorial/grpc/geodatas/rpc/search_servicer.py
@@ -1,11 +1,12 @@
 """
 
 """
+from geoalchemy2 import WKTElement
 import logging
 from sqlalchemy import func
 import statsd
 
-from tutorial.grpc.geodatas.models.models import Thing, session
+from tutorial.grpc.geodatas.models.models import Thing, session, OGRGeoJSON
 from tutorial.grpc.geodatas.proto import search_pb2, search_pb2_grpc
 
 
@@ -14,6 +15,31 @@ logger = logging.getLogger(__name__)
 
 class SearchServicer(search_pb2_grpc.SearchServicer):
     stat = statsd.StatsClient('localhost', 8125)
+
+    @staticmethod
+    def _search_thing(lat, lng):
+        query = session.query(Thing).filter(
+            func.ST_Contains(Thing.geom, 'POINT({} {})'.format(lat, lng)))
+        return [search_pb2.SearchResponse(response=rec.name) for rec in query]
+
+    @staticmethod
+    def _search_commune(lat, lng):
+        # https://gis.stackexchange.com/questions/189987/casting-between-postgis-types-in-geoalchemy
+        query = session.query(OGRGeoJSON).filter(
+            func.ST_Contains(OGRGeoJSON.wkb_geometry,
+                             "SRID=4326;POINT({} {})".format(lat, lng))
+        )
+
+        return [
+            search_pb2.SearchResponse(
+                response="{} - {} - {}".format(
+                    rec.insee,
+                    rec.nom,
+                    rec.wikipedia
+                )
+            )
+            for rec in query
+        ]
 
     @stat.timer("search")
     def search(self, request, _context):
@@ -25,10 +51,10 @@ class SearchServicer(search_pb2_grpc.SearchServicer):
         """
         self.stat.incr("search_count")
         logger.info("search request: " + str(request))
-        query = session.query(Thing).filter(
-            func.ST_Contains(Thing.geom, 'POINT({} {})'.format(request.lat, request.lng)))
-        responses = [search_pb2.SearchResponse(
-            response=rec.name) for rec in query]
+
+        # responses = self._search_thing(request.lat, request.lng)
+        responses = self._search_commune(request.lat, request.lng)
+
         logger.info("search responses: " + str(responses))
         return search_pb2.SearchResponses(responses=responses)
 
